86cba60dc9268c8e8596147acefaf47015c05c2d
Alexiuce <alexiuce@qq.com>
2018-02-23 19:47:39 +0800

    config ToolBarView

--- Example/Example/ToolBarView/ToolBarView.swift
+++ Example/Example/ToolBarView/ToolBarView.swift
@@ -17,23 +17,9 @@ class ToolBarView: NSView {
     
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
-        
-        guard let nib = NSNib(nibNamed: NSNib.Name.init(rawValue: "ToolBarView"), bundle: nil) else { return  }
-        
-        var topArray = [NSArray?]()
-        let pointer = AutoreleasingUnsafeMutablePointer<NSArray?>(&topArray)
-        print("befor load nib")
-        nib.instantiate(withOwner: self, topLevelObjects: pointer)
-//        Bundle.main.loadNibNamed(NSNib.Name.init(rawValue: "ToolBarView"), owner: self, topLevelObjects: pointer)
-        print("after load nib")
+        Bundle.main.loadNibNamed(NSNib.Name.init(rawValue: "ToolBarView"), owner: self, topLevelObjects: nil)
         view.frame = bounds
         self.addSubview(view)
-    
-        guard let views = pointer.pointee else { return  }
-        for view in views {
-            guard let v = view as? NSView else {continue}
-            print(v)
-        }
     }
     
     required init?(coder decoder: NSCoder) {
@@ -44,13 +30,6 @@ class ToolBarView: NSView {
         super.draw(dirtyRect)
 
         // Drawing code here.
-    }
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        print("awke  from nib ")
     }
     
     
