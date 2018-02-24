15b41c468b1813997ca31b782cd71e7adef57832
Alexiuce <alexiuce@qq.com>
2018-02-24 13:42:13 +0800

    load image from bundle

--- Classes/ToolBarView/ToolBarView.swift
+++ Classes/ToolBarView/ToolBarView.swift
@@ -33,6 +33,29 @@ public class ToolBarView: NSView {
         // Drawing code here.
     }
     
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+      
+        // 获取pod bundle
+       let podBundle =  Bundle(for: ToolBarView.self)
+        // 获取存储图片资源的bundle url
+        guard let toolImageBundleURL = podBundle.url(forResource: "ToolBarView", withExtension: "bundle") else {return}
+        // 根据URL 获取bundle
+        guard let toolImageBundle = Bundle(url: toolImageBundleURL)  else { return  }
+    
+        // 遍历所有btn
+        for button in view.subviews {
+            if let v = button as? NSButton {
+                // 设置图片
+                
+               v.image =  toolImageBundle.image(forResource: NSImage.Name("toolbar_emoticon_normal"))
+//                if let normalPath = toolImageBundle.pathForImageResource(NSImage.Name("toolbar_emoticon_down")) {
+//                 v.image = NSImage(contentsOfFile: normalPath)
+//                }
+            }
+        }
+    }
+    
     
     
 }
